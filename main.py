f47ef5160f021d0867d04367287f9235ea0eb525
Beatriz Rianho Bernadinho <pbeatriz18@hotmail.com.br>
2022-03-05 12:18:27 -0300

    versão 1.0

--- main.py
+++ main.py
@@ -1,54 +1,95 @@
+from distutils.log import error
 from os import execlp
 import sys
 
-exp = sys.argv[1]
 
-result = 0
-subs = []
+class Token:
 
-if exp[0] == ' ' and (exp[1] == "+" or exp[1] == "-") or ('+' not in exp and '-' not in exp):
-
-    sys.stderr.write('invalido')
-
-exp = exp.replace(" ", '')
-
-if '+' in exp:
-    if '-' in exp:
-
-        list = exp.split('+')
-
-        for i in list:
-            if '-' not in i:
-                result += int(i)
-            else:
-
-                subs.append(i)
-
-        subs = '+'.join(subs)
-
-    else:
-
-        list = exp.split('+')
-        for i in list:
-            result += int(i)
-
-if '-' in exp:
-
-    if len(subs) == 0:
-        list = exp.split('-')
-
-    else:
-
-        list = subs.split('-')
-        for i in list:
-            if '+' in i:
-                result += int(i.split('+')[-1])
-                result -= int(i.split('+')[0])
-                list.remove(i)
-
-    result += int(list[0])
-    for i in range(1, len(list)):
-        result -= int(list[i])
+    def __init__(self, value, type):
+        self.type = type
+        self.value = value
 
 
-print(result)
+class Tokenizer:
+
+    def __init__(self, origin):
+
+        self.origin = origin  # '1+2+3'
+        self.position = 0
+        self.actual = None
+        self.origin = self.origin.replace(" ", '')
+
+    def selectNext(self):
+
+        if self.position >= len(self.origin):
+
+            self.actual = Token(' ', "EOF")
+            return self.actual
+
+        elif self.origin[self.position] == '+':
+
+            self.position += 1
+            self.actual = Token('+', 'PLUS')
+            return self.actual
+
+        elif self.origin[self.position] == '-':
+
+            self.position += 1
+            self.actual = Token('-', 'MINUS')
+            return self.actual
+
+        elif self.origin[self.position].isdigit():
+            candidato = self.origin[self.position]
+
+            self.position += 1
+
+            while self.position < len(self.origin) and self.origin[self.position].isdigit():
+                candidato += self.origin[self.position]
+                self.position += 1
+
+            self.actual = Token(int(candidato), 'INT')
+            return self.actual
+
+        else:
+
+            raise ValueError('Invalid Token')
+
+
+class Parser:
+
+    tokens = None
+
+    def parseExpression():
+
+        token = Parser.tokens.selectNext()
+        resultado = 0
+
+        if token.type == 'INT':
+            resultado = token.value
+            token = Parser.tokens.selectNext()
+
+            while token.type == 'PLUS' or token.type == 'MINUS':
+                if token.type == 'PLUS':
+                    token = Parser.tokens.selectNext()
+
+                    if token.type == 'INT':
+                        resultado += token.value
+                    else:
+                        raise ValueError('Invalid Token')
+                elif token.type == 'MINUS':
+                    token = Parser.tokens.selectNext()
+                    if token.type == 'INT':
+                        resultado -= token.value
+                    else:
+                        raise ValueError('Invalid Token')
+                token = Parser.tokens.selectNext()
+            return resultado
+        else:
+            raise ValueError('Invalid Code')
+
+    def run(code):
+        Parser.tokens = Tokenizer(code)
+        return Parser.parseExpression()
+
+
+Parser.run(' 244 -4- 1 ')
